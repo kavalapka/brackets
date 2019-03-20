06b138317f660416b5000dd80768263f9f3eec3e
Katerina Zenchik <e.zenchik@gmail.com>
2019-03-20 15:50:59 +0300

    add cpompare brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+  const stack = [];
+  const check = str.split('');
+
+  for (let i in check) {
+    for(let br in bracketsConfig){
+      if (check[i] === bracketsConfig[br][0]){
+        if (bracketsConfig[br][0]=== bracketsConfig[br][1]) {
+          if (stack[stack.length -1] === check[i]) {
+            stack.pop();
+            continue
+          }
+        }
+        stack.push(check[i]);
+        continue
+      }
+
+      if(check[i] === bracketsConfig[br][1]) {
+        let open = stack.pop();
+        if (open !== bracketsConfig[br][0]) {
+          return false
+        }
+      }
+    }
+  }
+  
+  return stack.length === 0
+};
